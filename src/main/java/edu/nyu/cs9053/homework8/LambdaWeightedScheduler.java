d7c442495c27407319773008220cca6e488c5e76
Yingbing Wang <yw2848@nyu.edu>
2017-04-05 16:30:12 -0400

    edi weighted scheduler

--- src/main/java/edu/nyu/cs9053/homework8/LambdaWeightedScheduler.java
+++ src/main/java/edu/nyu/cs9053/homework8/LambdaWeightedScheduler.java
@@ -1,9 +1,10 @@
 package edu.nyu.cs9053.homework8;
 
+
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class LambdaWeightedScheduler extends LambdaScheduler{
 	private final ArrayList<LambdaJob> weightedScheduler;
@@ -24,8 +25,8 @@ public class LambdaWeightedScheduler extends LambdaScheduler{
 	public ArrayList<LambdaJob> scheduleWeightedJobs(){
 		Collections.sort(weightedScheduler);
 
-		double[] table = new double[weightedScheduler.size()];
-		table[0] = weightedScheduler.get(0).getProfit();
+		double[] jobTable = new double[weightedScheduler.size()];
+		jobTable[0] = weightedScheduler.get(0).getProfit();
 
 		HashMap<Double, Integer> hashMap = new HashMap<>();
 		hashMap.put(weightedScheduler.get(0).getProfit(), 0); 
@@ -35,10 +36,10 @@ public class LambdaWeightedScheduler extends LambdaScheduler{
 			double currProfit = weightedScheduler.get(i).getProfit();
 			int latestNonConflictJob = findLatestNonConflict(weightedScheduler, i);
 			if(latestNonConflictJob != -1){
-				currProfit += table[latestNonConflictJob];
+				currProfit += jobTable[latestNonConflictJob];
 			}
-			maxProfit = Math.max(currProfit, table[i - 1]);
-			table[i] = maxProfit;
+			maxProfit = Math.max(currProfit, jobTable[i - 1]);
+			jobTable[i] = maxProfit;
 
 			if(!hashMap.containsKey(maxProfit)){
 				hashMap.put(maxProfit, i);
@@ -50,7 +51,7 @@ public class LambdaWeightedScheduler extends LambdaScheduler{
 		return resultSet;
 	}
 
-	private ArrayList<LambdaJob> getOptimalJobs(ArrayList<LambdaJob> scheduler, HashMap<Double, Integer> hashMap, Double maxProfit){
+	public ArrayList<LambdaJob> getOptimalJobs(ArrayList<LambdaJob> scheduler, HashMap<Double, Integer> hashMap, Double maxProfit){
 		ArrayList<LambdaJob> resultSet = new ArrayList<>();
 		while(hashMap.get(maxProfit) != null){
 			LambdaJob temp = scheduler.get(hashMap.get(maxProfit));
@@ -60,7 +61,7 @@ public class LambdaWeightedScheduler extends LambdaScheduler{
 		return resultSet;
 	}
 
-	private int findLatestNonConflict(ArrayList<LambdaJob> jobList, int index){
+	public int findLatestNonConflict(ArrayList<LambdaJob> jobList, int index){
 		for(int j = index - 1; j >= 0; j--){
 			if(jobList.get(j).getFinish().compareTo(jobList.get(index).getStart()) <= 0){
 				return j;
